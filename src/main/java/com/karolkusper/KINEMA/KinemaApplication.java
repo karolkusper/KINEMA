5e0a344e73e80a97b9b50ea0377c6414620cc123
karolkusper <karolkusper@gmail.com>
2024-04-06 13:46:35 +0200

    create springboot app

---
+++ src/main/java/com/karolkusper/KINEMA/KinemaApplication.java
@@ -0,0 +1,13 @@
+package com.karolkusper.KINEMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karolkusper/KINEMA/KinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karolkusper.KINEMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
