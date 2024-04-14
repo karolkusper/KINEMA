37e43f9a0190c1913a3921ce763dc1650aa5bf6d
karolkusper <karolkusper@gmail.com>
2024-04-14 17:42:33 +0200

    Move backend-related files to backend folder

--- backend/src/main/java/com/karolkusper/KINEMA/dao/BookingRepository.java
+++ backend/src/main/java/com/karolkusper/KINEMA/dao/BookingRepository.java
@@ -3,7 +3,5 @@ package com.karolkusper.KINEMA.dao;
 import com.karolkusper.KINEMA.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Book;
-
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 }
