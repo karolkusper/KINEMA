061432239baa83ce514cbd60e56b3c8a810ee954
karolkusper <karolkusper@gmail.com>
2024-04-14 22:23:28 +0200

    change styles in Home.js and NavBar.jsx

--- frontend/KINEMA_FRONTEND/src/pages/Home.jsx
+++ frontend/KINEMA_FRONTEND/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import '../styles/Home.css'
-import BannerImage from '../assets/tasma.jpg'
+import BannerImage from '../assets/kino.jpg'
 function Home() {
   return (
     <div className='home'  style={{backgroundImage: `url(${BannerImage})`}}>
         <div className='headerContainer'>
             <h1>KINEMA - YOUR FAVOURITE CINEMA</h1>
         
-            <p>Explore world of cinematography...</p>
+            <p>Explore the world of cinematography...</p>
 
             <Link to='/movies'>
                 <button>
