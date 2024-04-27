570bdc925220a80b1e33562051b7529e079652a7
karolkusper <karolkusper@gmail.com>
2024-04-27 13:44:56 +0200

    Work with BookingServiceImpl, ScreeningServiceImpl, UserServiceImpl services

--- backend/src/main/java/com/karolkusper/KINEMA/service/Booking/BookingServiceImpl.java
+++ backend/src/main/java/com/karolkusper/KINEMA/service/Booking/BookingServiceImpl.java
@@ -1,30 +1,64 @@
 package com.karolkusper.KINEMA.service.Booking;
 
+import com.karolkusper.KINEMA.dao.BookingRepository;
 import com.karolkusper.KINEMA.entity.Booking;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BookingServiceImpl implements BookingService{
 
-    //TO DO
+    private final BookingRepository bookingRepository;
+
+    @Autowired
+    public BookingServiceImpl(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
     @Override
     public List<Booking> findAll() {
-        return null;
+        return bookingRepository.findAll();
     }
 
     @Override
     public Booking findById(int id) {
-        return null;
+        Booking booking = null;
+        Optional<Booking> result = bookingRepository.findById(id);
+        if(result.isPresent()){
+            booking=result.get();
+
+        }
+        else{
+            throw new RuntimeException("There is no booking with id="+id);
+        }
+        return booking;
     }
 
     @Override
     public Booking save(Booking booking) {
-        return null;
+        int id = booking.getId();
+
+        if(id==0){
+            return bookingRepository.save(booking);
+        }
+        else{
+            Booking existingBooking =bookingRepository.findById(id).orElseThrow(()->new RuntimeException("There is no booking with id="+id));
+
+            existingBooking.setUser(booking.getUser());
+            existingBooking.setScreening(booking.getScreening());
+            existingBooking.setTicketType(booking.getTicketType());
+            existingBooking.setSeatIds(booking.getSeatIds());
+
+            return  bookingRepository.save(existingBooking);
+
+        }
     }
 
     @Override
     public void deleteById(int id) {
-
+        System.out.println("Usuwanie booking id="+id);
     }
 }
--- backend/src/main/java/com/karolkusper/KINEMA/service/Screening/ScreeningServiceImpl.java
+++ backend/src/main/java/com/karolkusper/KINEMA/service/Screening/ScreeningServiceImpl.java
@@ -1,31 +1,70 @@
 package com.karolkusper.KINEMA.service.Screening;
 
+import com.karolkusper.KINEMA.dao.ScreeningRepository;
 import com.karolkusper.KINEMA.entity.Screening;
+import com.karolkusper.KINEMA.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ScreeningServiceImpl implements ScreeningService{
 
+    private final ScreeningRepository screeningRepository;
+
+    @Autowired
+    public ScreeningServiceImpl(ScreeningRepository repository) {
+        this.screeningRepository = repository;
+    }
+
     //TO DO
     @Override
     public List<Screening> findAll() {
-        return null;
+        return screeningRepository.findAll();
     }
 
     @Override
     public Screening findById(int id) {
-        return null;
+        Screening screening = null;
+        Optional<Screening> result = screeningRepository.findById(id);
+
+        if(result.isPresent()){
+            screening=result.get();
+        }
+        else{
+            throw new RuntimeException("Did not find screening with this id: "+id);
+        }
+        return screening;
     }
 
     @Override
-    public Screening save(Screening movie) {
-        return null;
+    public Screening save(Screening screening) {
+        int id = screening.getId();
+        if(id==0){
+            return screeningRepository.save(screening);
+        }
+        else{
+            Screening existingScreening = screeningRepository.findById(id)
+                    .orElseThrow(()->new RuntimeException("There is no screening with id="+id));
+
+            existingScreening.setCinemaHall(screening.getCinemaHall());
+            existingScreening.setMovie(screening.getMovie());
+            existingScreening.setStartTime(screening.getStartTime());
+            existingScreening.setEndTime(screening.getEndTime());
+            existingScreening.setFormat(screening.getFormat());
+
+            return screeningRepository.save(existingScreening);
+        }
     }
+
+
 
     @Override
     public void deleteById(int id) {
-
+        //to do
+        System.out.println("Usuwanie screening id="+id);
     }
 }
