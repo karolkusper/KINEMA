f65cadd072c2b0e48816749fd7cf753e42436767
karolkusper <karolkusper@gmail.com>
2024-04-28 18:18:48 +0200

    Create Slider component in Movies page

--- frontend/KINEMA_FRONTEND/src/pages/Movies.jsx
+++ frontend/KINEMA_FRONTEND/src/pages/Movies.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import {MovieList} from '../variables/MovieList'
 import MovieItem from '../components/MovieItem'
+import Slider from '../components/Slider'
 import '../styles/Movies.css'
 function Movies() {
   return (
     <div className='movies'>
         <h1 className='moviesTitle'>Our movies</h1>
+        <div className='sliderDiv'>
+          <Slider />
+        </div>
+        
         <div className='moviesList'>
             {MovieList.map((movie,key)=>{
                 return <MovieItem 
