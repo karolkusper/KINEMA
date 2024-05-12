a027f06e30a8f740194d8ce760311c5bd8040b4a
karolkusper <karolkusper@gmail.com>
2024-05-12 23:08:23 +0200

    movies page takes data from backend

--- frontend/KINEMA_FRONTEND/src/components/Slider.jsx
+++ frontend/KINEMA_FRONTEND/src/components/Slider.jsx
@@ -1,39 +1,96 @@
-import React from 'react'
-import { useState } from 'react';
-import {MovieList} from '../variables/MovieList'
-import "../styles/Slider.css"
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import "../styles/Slider.css";
 
-function Slider() {
-    const [currentIndex,setCurrentIndex] = useState(0);
-    const goToPrevious = ()=>{
-        const isFirstSlide = currentIndex===0;
-        const newIndex = isFirstSlide ? MovieList.length-1 : currentIndex-1;
+function Slider({ movies }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const goToPrevious = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? movies.length - 1 : currentIndex - 1;
         setCurrentIndex(newIndex);
-    }
+    };
 
-    const goToNext = ()=>{
-        const isLastSlide = currentIndex===MovieList.length-1;
-        const newIndex = isLastSlide ? 0 : currentIndex+1;
+    const goToNext = () => {
+        const isLastSlide = currentIndex === movies.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
         setCurrentIndex(newIndex);
-    }
+    };
 
-    const goToSlide = (index)=>{
+    const goToSlide = (index) => {
         setCurrentIndex(index);
+    };
+
+    
+    if (!movies || movies.length === 0) {
+        return null; 
     }
-  return (
-    <div className='sliderStyles'>
-        <div className='arrow left' onClick={goToPrevious}>❰</div>
-        <div className='arrow right' onClick={goToNext}>❱</div>
 
-        <div className='slideStyles' style={{backgroundImage: `url(${MovieList[currentIndex].image})`}}></div>
 
-        <div className='dotsContainer'>
-            {MovieList.map((movie,key)=>{
-               return <div className='dots' key={key} onClick={()=>goToSlide(key)}>●</div>
-            })}
+    const currentMovie = movies[currentIndex] || movies[0]; 
+
+    return (
+        <div className='sliderStyles'>
+            <div className='arrow left' onClick={goToPrevious}>❰</div>
+            <div className='arrow right' onClick={goToNext}>❱</div>
+
+            {/* Używamy currentMovie, aby upewnić się, że mamy dostęp do plakatu (posterPath) */}
+            <div className='slideStyles' style={{ backgroundImage: `url(${currentMovie.posterPath})` }}></div>
+
+            <div className='dotsContainer'>
+                {movies.map((movie, key) => (
+                    <div className='dots' key={key} onClick={() => goToSlide(key)}>●</div>
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Slider
+Slider.propTypes = {
+    movies: PropTypes.arrayOf(
+        PropTypes.shape({
+            posterPath: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
+export default Slider;
+// import React from 'react'
+// import { useState } from 'react';
+// import {MovieList} from '../variables/MovieList'
+// import "../styles/Slider.css"
+
+// function Slider() {
+//     const [currentIndex,setCurrentIndex] = useState(0);
+//     const goToPrevious = ()=>{
+//         const isFirstSlide = currentIndex===0;
+//         const newIndex = isFirstSlide ? MovieList.length-1 : currentIndex-1;
+//         setCurrentIndex(newIndex);
+//     }
+
+//     const goToNext = ()=>{
+//         const isLastSlide = currentIndex===MovieList.length-1;
+//         const newIndex = isLastSlide ? 0 : currentIndex+1;
+//         setCurrentIndex(newIndex);
+//     }
+
+//     const goToSlide = (index)=>{
+//         setCurrentIndex(index);
+//     }
+//   return (
+//     <div className='sliderStyles'>
+//         <div className='arrow left' onClick={goToPrevious}>❰</div>
+//         <div className='arrow right' onClick={goToNext}>❱</div>
+
+//         <div className='slideStyles' style={{backgroundImage: `url(${MovieList[currentIndex].image})`}}></div>
+
+//         <div className='dotsContainer'>
+//             {MovieList.map((movie,key)=>{
+//                return <div className='dots' key={key} onClick={()=>goToSlide(key)}>●</div>
+//             })}
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Slider
--- frontend/KINEMA_FRONTEND/src/pages/Movies.jsx
+++ frontend/KINEMA_FRONTEND/src/pages/Movies.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
-import {MovieList} from '../variables/MovieList'
-import MovieItem from '../components/MovieItem'
-import Slider from '../components/Slider'
-import '../styles/Movies.css'
+import React, { useState, useEffect } from 'react';
+import MovieItem from '../components/MovieItem';
+import Slider from '../components/Slider';
+import '../styles/Movies.css';
+import api from "../axiosAuth";
+
 function Movies() {
-  return (
-    <div className='movies'>
-        <h1 className='moviesTitle'>Our movies</h1>
-        <div className='sliderDiv'>
-          <Slider />
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await api.get('/api/movies');
+                setMovies(response.data);
+            } catch (error) {
+                console.log('Error fetching movies:', error);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
+    return (
+        <div className='movies'>
+            <h1 className='moviesTitle'>Our movies</h1>
+            <div className='sliderDiv'>
+                <Slider movies={movies} />
+            </div>
+
+            <div className='moviesList'>
+                {movies.map((movie) => (
+                    <MovieItem
+                        key={movie.id}
+                        title={movie.title}
+                        desc={movie.description}
+                        director={movie.director}
+                        productionYear={movie.productionYear}
+                        // image={movie.image} // Assuming you have an 'image' property in your Movie model
+                        image={movie.posterPath}
+                    />
+                ))}
+            </div>
         </div>
-        
-        <div className='moviesList'>
-            {MovieList.map((movie,key)=>{
-                return <MovieItem 
-                key={key}
-                title={movie.title}
-                desc={movie.desc}
-                director={movie.director}
-                productionYear={movie.rok_produkcji}
-                image={movie.image}
-                />
-            })}
-        </div>
-    </div>
-  )
+    );
 }
 
-export default Movies
+export default Movies;
