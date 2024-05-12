1ba29093129fcd1ab011c8eb3b98a0135f6c8d0c
karolkusper <karolkusper@gmail.com>
2024-05-12 20:20:21 +0200

    add logout button to navbar, more work on styles(change some colors)

--- frontend/KINEMA_FRONTEND/src/components/NavBar.jsx
+++ frontend/KINEMA_FRONTEND/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import Logo from "../assets/cinema_logo.png";
-import {Link} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import '../styles/NavBar.css'
 import MenuButton from './MenuButton';
 
@@ -8,12 +8,27 @@ import ReorderIcon from '@mui/icons-material/Reorder';
 
 function NavBar() {
 
+    const navigate = useNavigate();
+
     const [openLinks, setOpenLinks] = useState(false);
 
     const toggleNavBar = ()=>{
         setOpenLinks(!openLinks);
     }
 
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        setToken(storedToken);
+    }, []);
+
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setToken('');
+        navigate('/content/home');
+    }
   return (
     <div className='navbar'>
         <div className='leftSide' id={openLinks ? 'open' : 'close'}>
@@ -29,7 +44,12 @@ function NavBar() {
             <MenuButton desc="/content/home" name="Home"/>
             <MenuButton desc="/content/movies" name="Movies"/>
             <MenuButton desc="/content/repertuar" name="Repertuar"/>
-            <MenuButton desc="/content/contact" name="Contact"/>
+            {token && <MenuButton desc="/content/profile" name="Profile"/>}
+            {token ? (
+                    <button className="menuButton" onClick={logout}>Logout</button>
+                ) : (
+                    <MenuButton desc="/" name="Login"/>
+                )}
             <button onClick={toggleNavBar}>
                 <ReorderIcon />
             </button>
