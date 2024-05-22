5752c86755690036eba20670f325a149f63c3d3f
karolkusper <karolkusper@gmail.com>
2024-05-22 18:38:04 +0200

    Change SeatPicker to work on seat-avaibility api

--- frontend/KINEMA_FRONTEND/src/components/SeatPicker.jsx
+++ frontend/KINEMA_FRONTEND/src/components/SeatPicker.jsx
@@ -3,13 +3,13 @@ import Seat from './Seat';
 import api from '../axiosAuth';
 import '../styles/SeatPicker.css';
 
-const SeatPicker = ({ cinemaHallId }) => {
+const SeatPicker = ({ screeningId }) => {
   const [seats, setSeats] = useState([]);
 
   useEffect(() => {
     const fetchSeats = async () => {
       try {
-        const response = await api.get(`/api/seats/${cinemaHallId}`);
+        const response = await api.get(`/api/seat-availability/${screeningId}`);
         setSeats(response.data);
       } catch (error) {
         console.error('Error fetching seats:', error);
@@ -17,20 +17,19 @@ const SeatPicker = ({ cinemaHallId }) => {
     };
 
     fetchSeats();
-  }, [cinemaHallId]);
+  }, [screeningId]);
 
   const rows = {};
   seats.forEach(seat => {
-    if (!rows[seat.seatRow]) {
-      rows[seat.seatRow] = [];
+    if (!rows[seat.seat.seatRow]) {
+      rows[seat.seat.seatRow] = [];
     }
-    rows[seat.seatRow].push(seat);
+    rows[seat.seat.seatRow].push(seat);
   });
-
 
   const reserveSeat = async (seatId) => {
     try {
-      const response = await api.post(`/api/seats/hall/${seatId}`);
+      const response = await api.post(`/api/seat-availability/reserve`, { screeningId, seatId });
       const updatedSeat = response.data;
       setSeats(prevSeats => 
         prevSeats.map(seat => seat.id === updatedSeat.id ? updatedSeat : seat)
@@ -39,7 +38,6 @@ const SeatPicker = ({ cinemaHallId }) => {
       console.error('Error reserving seat:', error);
     }
   };
-
 
   return (
     <div className="seat-picker-container">
@@ -51,10 +49,10 @@ const SeatPicker = ({ cinemaHallId }) => {
               {rows[row].map(seat => (
                 <Seat 
                   key={seat.id}
-                  row={seat.seatRow}
-                  column={seat.seatColumn}
-                  isAvailable={seat.available}
-                  onClick={() => reserveSeat(seat.id)}
+                  row={seat.seat.seatRow}
+                  column={seat.seat.seatColumn}
+                  isAvailable={seat.isAvailable}
+                  onClick={() => reserveSeat(seat.seat.id)}
                 />
               ))}
             </div>
