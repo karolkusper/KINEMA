7cceb2f769528b8a33145b11175a091c37815064
karolkusper <karolkusper@gmail.com>
2024-06-03 18:57:13 +0200

    Work with screenings styles

--- frontend/KINEMA_FRONTEND/src/components/ScreeningRow.jsx
+++ frontend/KINEMA_FRONTEND/src/components/ScreeningRow.jsx
@@ -40,31 +40,3 @@ function ScreeningRow({ screeningId, cinemaHall, movie, startTime, endTime, form
 }
 
 export default ScreeningRow;
-
-
-// import React from 'react';
-// import "../styles/ScreeningRow.css";
-// import ScreeningTimeButton from './ScreeningTimeButton';
-
-// function ScreeningRow({ screeningId,cinemaHall, movie, startTime, endTime, format }) {
-//   // Obliczamy czas trwania filmu
-//   const duration = Math.floor((endTime - startTime) / (1000 * 60));
-
-//   // Formatowanie daty/godziny
-//   const formattedStartTime = new Date(startTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-//   return (
-//     <div className='screeningRowContainer'>
-//       <div className='screeningRow-image' style={{ backgroundImage: `url(${movie.posterPath})` }}></div>
-//       <div className='screeningRow-info'>
-//         <h1>{movie.title}</h1>
-//         <h3>{movie.movieGenre} | {duration} min | {format} | Hall: {cinemaHall.hallName}</h3>
-//         <ScreeningTimeButton time={formattedStartTime} screeningId={screeningId}/>
-//         <h2>Director: {movie.director} | Production year: {movie.productionYear}</h2>
-//         <p>{movie.description}</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ScreeningRow;
